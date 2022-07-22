f2fc3a591f3436b0df4e89ce05f3e4d580145390
Devendra kumar <devendrapal8682@gmail.com>
2022-07-22 20:06:44 +0530

    React Startd

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
-
+let name = "Devendra kumar "
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <nav>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+    </nav>
+    <div className="container">
+      <h1>Hello {name}</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente pariatur, nesciunt sit ipsa incidunt corrupti quas placeat suscipit ducimus consectetur, iusto alias magni optio odit officia, qui dignissimos maxime dolorem!</p>
     </div>
+    </>
   );
 }
 
